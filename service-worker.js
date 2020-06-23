83802e813b80191a6ec04d3cfb9f7b528d1b74b9
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-06-23 14:33:14 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aaronl647/tip-calculator/precache-manifest.53c7641cc24647ee0c4edf3879ec99a8.js"
+  "/aaronl647.github.io/tip-calculator/precache-manifest.4a5d7b852620adc6a66b97277dab39fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aaronl647/tip-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aaronl647.github.io/tip-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
